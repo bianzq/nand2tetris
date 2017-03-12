e414be10b62636f3392e5a6388bda05502bcf597
xiaoxd16 <xiaoxd16@163.com>
2017-03-13 01:05:24 +0800

    p04

--- 肖迅东/projects/04/fill/Fill.asm
+++ 肖迅东/projects/04/fill/Fill.asm
@@ -12,3 +12,53 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+//j1      j2          j3
+//(out < 0)(out = 0)(out > 0) Mnemonic     Effect
+//   0         0        0       null      No jump
+//   0         0        1       JGT    If out > 0 jump
+//   0         1        0       JEQ    If out = 0 jump
+//   0         1        1       JGE    If out b 0 jump
+//   1         0        0       JLT    If out < 0 jump
+//   1         0        1       JNE    If out !=0 jump
+//   1         1        0       JLE    If out a 0 jump
+//   1         1        1       JMP        Jump
+
+(WHILE)
+@KBD
+D=M
+@i
+M=0
+@ELSE
+D;JEQ
+(IF)
+@i
+D=M
+@SCREEN
+A=A+D
+M=-1
+@i
+M=M+1
+D=M
+D=M+D
+@SCREEN
+D=A-D
+@IF
+D;JGT
+@WHILE
+D;JEQ
+(ELSE)
+@i
+D=M
+@SCREEN
+A=A+D
+M=0
+@i
+M=M+1
+D=M
+D=M+D
+@SCREEN
+D=A-D
+@ELSE
+D;JGT
+@WHILE
+D;JEQ
--- 肖迅东/projects/04/mult/mult.asm
+++ 肖迅东/projects/04/mult/mult.asm
@@ -7,3 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//JGT >0 to end
+//JNG <0 to endWhile
+//JMP goto loop
+//x;xxx if x = 0 ; xxx
+//j1      j2          j3
+//(out < 0)(out = 0)(out > 0) Mnemonic     Effect
+//   0         0        0       null      No jump
+//   0         0        1       JGT    If out > 0 jump
+//   0         1        0       JEQ    If out = 0 jump
+//   0         1        1       JGE    If out b 0 jump
+//   1         0        0       JLT    If out < 0 jump
+//   1         0        1       JNE    If out !=0 jump
+//   1         1        0       JLE    If out a 0 jump
+//   1         1        1       JMP        Jump
+
+@R0
+D=M
+@a
+M=D
+@R1
+D=M
+@b
+M=D
+@sum
+M=0
+(LOOP)
+@a
+D=M
+@END
+D;JEQ
+@b
+D=M
+@sum
+M=D+M
+@a
+M=M-1
+@LOOP
+0;JMP
+(END)
+@sum
+D=M
+@R2
+M=D
+(WHILE)
+@WHILE
+0;JMP
